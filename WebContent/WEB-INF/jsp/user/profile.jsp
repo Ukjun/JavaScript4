7b1d648dcd9fd000af2ae96f5bc233d4419c5ad2
Ukjun <corn1107@hanail.net>
2020-08-31 17:17:04 +0900

    2020-08-31 프로필 변경

--- WebContent/WEB-INF/jsp/user/profile.jsp
+++ WebContent/WEB-INF/jsp/user/profile.jsp
@@ -1,6 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-    <link href="https://fonts.googleapis.com/css2?family=Gamja+Flower&family=Nanum+Pen+Script&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Gamja+Flower&family=Nanum+Pen+Script&display=swap" rel="stylesheet">
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+    
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,11 +10,22 @@
 <title>Insert title here</title>
 <style>
 *{font-family: 'Gamja Flower', cursive; font-size: 25px}
+img{width:96px; height: 96px; border-radius: 50%;}
 </style>
+	
 </head>
 <body>
 	<div>
-		<img src  ="${data.profile_img==null ? '/img/default_profile.jpg': '' }" width="400" height="400">
+		<c:choose>
+				<c:when test="${data.profile_img != null}" >
+
+					<!-- 로그인했을때 정보는 Const에 저장한 정보를 가져와야된다. -->
+					<img src="/img/user/${LoginUser.i_user }/${data.profile_img}" >
+				</c:when>
+				<c:otherwise>
+					<img src="/img/default_profile.jpg">
+				</c:otherwise>
+		</c:choose>
 	</div>
 	<div>
 		<form action="/profile" method="post" enctype="multipart/form-data">
--- src/com/koreait/pjt/user/ProfileSer.java
+++ src/com/koreait/pjt/user/ProfileSer.java
@@ -26,8 +26,8 @@ public class ProfileSer extends HttpServlet {
        
     //프로필 화면 ( 나의 프로필 이미지, 이미지 변경가능한 화면)
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		UserVO loginuser = MyUtils.getLoginUser(request);
-		request.setAttribute("data", UserDAO.selUser(loginuser.getI_user()));
+		UserVO loginUser = MyUtils.getLoginUser(request);		
+		request.setAttribute("data", UserDAO.selUser(loginUser.getI_user()));
 		
 		ViewResolver.forward("user/profile", request, response);
 	}
@@ -35,7 +35,7 @@ public class ProfileSer extends HttpServlet {
 	//이미지 변경 처리
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		UserVO loginUser =MyUtils.getLoginUser(request); 
-		String savePath = request.getSession().getServletContext().getRealPath("img") + "/user/" + loginUser.getI_user();
+		String savePath = getServletContext().getRealPath("img") + "/user/" + loginUser.getI_user();
 		System.out.println("savePath : " + savePath);
 		
 		File dir = new File(savePath);
@@ -49,26 +49,39 @@ public class ProfileSer extends HttpServlet {
 		
 		int maxFileSize = 10_485_760; //1024 * 1024 * 10(10mb) // 최대 파일 사이즈 크기
 		String fileNm = "";
-		String originFileNm = "";
+		String saveFileNm = null;
 		
 		try {
+			//MultipartRequest 웹페이지에서 파일 업로드할때 쓰는 객체 
+			//DefaultFileRenamePolicy 파일이름 생성시 중복방지
 			MultipartRequest multi = new MultipartRequest(request, savePath, maxFileSize, "UTF-8", new DefaultFileRenamePolicy());
-			
-			Enumeration files =multi.getFileNames();
+			Enumeration files = multi.getFileNames();
 			
 			while(files.hasMoreElements()) { // Element가 더 있는지 없는지 확인 
 				String key = (String)files.nextElement(); //키값이 넘어온다
 				fileNm = multi.getFilesystemName(key);
-				originFileNm = multi.getOriginalFileName(key);
+				//saveFileNm = multi.getOriginalFileName(key);
+				String fileExt = fileNm.substring(fileNm.lastIndexOf("."));
+				
+				//파일 확장자명 가져오기
+				//문자열의 인덱스값(왼쪽기준) : ~.indexOf();
+				//오른쪽 기준 : ~.lastIndexOf();
+				System.out.println("fileExt: " + fileExt);
+				
+				saveFileNm = UUID.randomUUID() +fileExt;
+				
 				
 				System.out.println("str: " + key);
 				System.out.println("fileNm: " + fileNm);
-				System.out.println("originFileNm: " + originFileNm);
+				System.out.println("saveFileNm: " + saveFileNm);
+				
+				
 				
 				//파일을 저장시키고 올릴때 oldFile이름이 newFile이름으로 덮어지게된다.
 				//UUID.randomUUID() : 파일이름을 랜덤으로 정하게 된다 (중복 방지 + 매번 다르게 이름이 지어진다.)
 				File oldFile = new File(savePath + "/" +fileNm);
-				File newFIle = new File(savePath + "/" + UUID.randomUUID()+".jpg");
+				//파일을 저장할때 이미지이름 -> 확장자까지 DB에 저장
+				File newFIle = new File(savePath + "/" + saveFileNm);
 				
 				oldFile.renameTo(newFIle);
 				
@@ -80,6 +93,13 @@ public class ProfileSer extends HttpServlet {
 			e.printStackTrace();
 		}
 		
+		if(saveFileNm != null) {
+			UserVO param = new UserVO();
+			param.setProfile_img(saveFileNm);
+			param.setI_user(loginUser.getI_user());
+			UserDAO.updUser(param);
+		}
+		response.sendRedirect("/profile");
 	}
 
 }
